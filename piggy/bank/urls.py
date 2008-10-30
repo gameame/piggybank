b0348a2385dff6c447c7ad11eb29cdc7824032c5
Gabriele Mearelli <g.mearelli@gmail.com>
2008-10-30 16:39:59 +0100

    I18N, static pages

--- piggy/bank/urls.py
+++ piggy/bank/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls.defaults import *
-from piggy import settings
 
-urlpatterns = patterns('',
+urlpatterns = patterns('piggy.bank.views',
     # Example:
-    (r'^$', 'piggy.bank.views.index'),
-    (r'^(?P<piggy_id>\d+)$', 'piggy.bank.views.detail'),
-    (r'^(?P<piggy_id>\d+)/donate$', 'piggy.bank.views.donate'),
-    
-    
-    # Development environment
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.MEDIA_ROOT}),
+    (r'^$', 'index'),
+    (r'^(?P<piggy_id>\d+)$', 'detail'),
+    (r'^(?P<piggy_id>\d+)/donate$', 'donate'),
+
 )
--- piggy/urls.py
+++ piggy/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import *
+from piggy import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    (r'^piggy/', include('piggy.bank.urls')),
+    (r'^(?P<language_code>[a-z]{2})/piggy/', include('piggy.bank.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
@@ -15,5 +15,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/(.*)', admin.site.root),
     
+    (r'^i18n/', include('django.conf.urls.i18n')),
 
+    (r'^(?P<language_code>[a-z]{2})/', include('piggy.home.urls')),
+
+    # Development environment
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
 )
